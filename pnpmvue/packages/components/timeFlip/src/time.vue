5272c7726dd0ad8ed63510c2968b3e53d1e58c24
Mr-luoxi <940280747@qq.com>
2022-12-29 21:09:28 +0800

    123123123

--- pnpmvue/packages/components/timeFlip/src/time.vue
+++ pnpmvue/packages/components/timeFlip/src/time.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: luo xi
  * @Date: 2022-12-28 22:51:30
- * @LastEditTime: 2022-12-28 23:31:07
+ * @LastEditTime: 2022-12-29 21:07:53
  * @LastEditors: luo xi
  * @Description: 
  * @FilePath: /KeepCoding/pnpmvue/packages/components/timeFlip/src/time.vue
@@ -22,27 +22,53 @@
 
 <script setup>
 import { timeProps } from "./timeFlip.js";
-import { ref } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 // import TimeFlip from "./timeFlip.vue";
 import { useNamespace } from "@iep-plus/hooks";
+import dayjs from "dayjs";
 
 defineProps(timeProps);
-
-const config = ref({});
-const count = ref([1, 2, 3, 4, 5, 6]);
-const curCount = ref([1, 2, 3, 4, 5, 6]);
-const ns = useNamespace("time");
-
-setTimeout(() => {
-  count.value = [4, 5, 1, 1, 1, 1];
-}, 1000);
-
-setTimeout(() => {
-  count.value = [2, 3, 4, 11, 11, 11];
-}, 3000);
-
 defineOptions({
   name: "timeBlock",
   inheritAttrs: false,
 });
+
+const config = ref({});
+const count = ref([]);
+const curCount = ref([1, 2, 3, 4, 5, 6]);
+const nowTime = ref(new Date());
+const ns = useNamespace("time");
+
+function getTime() {
+  const hour = nowTime.value.getHours().toString().split("");
+  const minute = nowTime.value.getMinutes().toString().split("");
+  const second = nowTime.value.getSeconds().toString().split("");
+
+  if (hour.length === 1) {
+    hour.unshift(0);
+  }
+  if (minute.length === 1) {
+    minute.unshift(0);
+  }
+  if (second.length === 1) {
+    second.unshift(0);
+  }
+
+  count.value[0] = Number(hour[0]);
+  count.value[1] = Number(hour[1]);
+  count.value[2] = Number(minute[0]);
+  count.value[3] = Number(minute[1]);
+  count.value[4] = Number(second[0]);
+  count.value[5] = Number(second[1]);
+}
+getTime();
+
+onMounted(() => {
+  setInterval(() => {
+    nowTime.value = new Date();
+    getTime();
+  }, 1000);
+});
+
+console.log(dayjs(new Date()).format("hh:mm:ss"));
 </script>
