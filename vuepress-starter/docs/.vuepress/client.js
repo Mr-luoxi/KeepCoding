60d5b200ef55503262fd79ee7c28e4cf078f6847
Mr-luoxi <940280747@qq.com>
2023-01-11 21:14:13 +0800

    新增md网站代码

---
+++ vuepress-starter/docs/.vuepress/client.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: luo xi
+ * @Date: 2023-01-11 21:05:15
+ * @LastEditTime: 2023-01-11 21:05:20
+ * @LastEditors: luo xi
+ * @Description:
+ * @FilePath: /KeepCoding/vuepress-starter/docs/.vuepress/client.js
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+import { defineClientConfig } from "@vuepress/client";
+
+export default defineClientConfig({
+  enhance({ app, router, siteData }) {},
+  setup() {},
+  rootComponents: [],
+});
---
+++ vuepress-starter/docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: luo xi
+ * @Date: 2023-01-11 21:04:37
+ * @LastEditTime: 2023-01-11 21:04:39
+ * @LastEditors: luo xi
+ * @Description:
+ * @FilePath: /KeepCoding/vuepress-starter/docs/.vuepress/config.js
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+import { defineUserConfig } from "vuepress";
+
+export default defineUserConfig({
+  lang: "zh-CN",
+  title: "你好， VuePress ！",
+  description: "这是我的第一个 VuePress 站点",
+});
